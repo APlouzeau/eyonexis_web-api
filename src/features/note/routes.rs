8536f05738d06e3fb6c853e797b0a122afd75563
Alexandre Alexandre <plouzeau.alexandre@outlook.com>
2026-09-14 21:45:41 +0200

    wip blocks

--- src/features/note/routes.rs
+++ src/features/note/routes.rs
@@ -15,4 +15,5 @@ pub fn routes() -> Router<AppState> {
         .route("/get-note/{id_note}", get(read::get_note_by_id))
         .route("/create", post(write::create))
         .route("/show/note/{*note}", get(read::get_note_by_path))
+        .route("/show/edit/note/{*note}", get(read::get_note_by_path))
 }
